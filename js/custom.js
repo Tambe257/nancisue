7983abccab935ff3f2f08564582eb8407170c7eb
Tony Tambe <tambe257@gmail.com>
2016-05-04 22:37:27 -0400

    Added nanci sue page

--- js/custom.js
+++ js/custom.js
@@ -22,35 +22,34 @@ $(document).on('scroll', function() {
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 });
 
 /* Animate third section on scroll */
 $(function() {
-    $(window).scroll( function(){
-        $('#third-section').each( function(){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* Adjust the "50" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 10;  
-          
-            if( bottom_of_window > bottom_of_object ){
-                
-                $('div#box1').addClass('animate3');
-                $('div#box2').addClass('animate2');
-                $('div#box3').addClass('animate1');
-            }
-        }); 
-    
-    });
+  $(window).scroll( function(){
+    $('#third-section').each( function(){
+
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* Adjust the "50" to either have a delay or that the content starts fading a bit before you reach it  */
+      bottom_of_window = bottom_of_window + 10;  
+
+      if( bottom_of_window > bottom_of_object ){
+
+          $('div#box1').addClass('animate3');
+          $('div#box2').addClass('animate2');
+          $('div#box3').addClass('animate1');
+      }
+    }); 
+  });
 });
 
 /* box slider */
